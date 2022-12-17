bd82ece1f39e4d0db656472821f0504ade8bb46c
Danielle <daniellec0321@gmail.com>
2022-12-17 11:57:45 -0600

    Wrote day01 code

--- day01/day01.py
+++ day01/day01.py
@@ -1,2 +1,42 @@
+def day01():
+
+    # set variables
+    max_calories = 0
+    curr_sum = 0
+
+    # open file
+    rfile = open("puzzle_input.txt", "r")
+
+    curr = rfile.readline()
+
+    # loop and read lines
+    while curr:
+
+        curr = curr.rstrip()
+    
+        # if at end of calorie sector
+        if (curr == ''):
+            
+            # test current sum against max
+            if (curr_sum > max_calories):
+                max_calories = curr_sum
+
+            # reset curr_sum
+            curr_sum = 0
+
+        # add to currsum
+        else:
+            curr_sum += int(curr)
+
+        # read next line
+        curr = rfile.readline()
+
+    # test last sum
+    if curr_sum > max_calories:
+        max_calories = curr_sum
+
+    return max_calories
+
 if __name__=='__main__':
     print("Advent of Code: Day 1")
+    print("Answer is " + str(day01()))
