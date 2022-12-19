c3e1c696d4323692cc2fcab3f11ab52362971b60
Danielle <daniellec0321@gmail.com>
2022-12-19 09:22:53 -0500

    Day04: Finished part 1

--- day04/day04.py
+++ day04/day04.py
@@ -1,2 +1,28 @@
+def day04_part_one():
+
+    ret = 0
+    rfile = open("puzzle_input.txt", "r")
+    curr = rfile.readline().rstrip()
+
+    while curr:
+
+        e1zone1 = int((curr.split(',')[0]).split('-')[0])
+        e1zone2 = int((curr.split(',')[0]).split('-')[1])
+        e2zone1 = int((curr.split(',')[1]).split('-')[0])
+        e2zone2 = int((curr.split(',')[1]).split('-')[1])
+
+        # e1 fully contains e2
+        if e1zone1 <= e2zone1 and e1zone2 >= e2zone2:
+            ret += 1
+        elif e2zone1 <= e1zone1 and e2zone2 >= e1zone2:
+            ret += 1
+
+        curr = rfile.readline().rstrip()
+
+    return ret
+
+
+
 if __name__=='__main__':
     print("Advent of Code: Day 04")
+    print("Part One answer is " + str(day04_part_one()))
