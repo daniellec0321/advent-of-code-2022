7aae13394c38a5472ec2337d2a5fd094b0b0d3ad
Danielle <daniellec0321@gmail.com>
2022-12-19 12:22:11 -0500

    Dy06: Finished part 1

--- day06/day06.py
+++ day06/day06.py
@@ -1,2 +1,20 @@
+def day06_part_one():
+
+    # read in the line
+    rfile = open("puzzle_input.txt", "r")
+    line = list(rfile.readline().rstrip())
+
+    for i in range(3, len(line)):
+
+        curr_packet = line[i-3:i+1]
+
+        if (len(curr_packet) == len(set(curr_packet))):
+            return i+1
+
+    return -1
+
+
+
 if __name__=='__main__':
     print("Advent of Code: Day 06")
+    print("Part One answer is " + str(day06_part_one()))
